edfb063143cc70b2b8cc3c9f14b9933aeb0a8bd5
Rithik Gavvala <gavvalarithik@gmail.com>
2020-07-29 01:04:40 -0400

    Updates

---
+++ precache-manifest.2333bafb1ee1c8ddf628924e77fb4fb4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ab143be33c201836b904145ac0450a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e6251040312d02322b1",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "99b3bb23390b48fcd2b9",
+    "url": "/portfolio/static/css/main.ce4bca1e.chunk.css"
+  },
+  {
+    "revision": "1e6251040312d02322b1",
+    "url": "/portfolio/static/js/2.865dd6f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.865dd6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b3bb23390b48fcd2b9",
+    "url": "/portfolio/static/js/main.2d15a994.chunk.js"
+  },
+  {
+    "revision": "361b25be0752ed4bc489",
+    "url": "/portfolio/static/js/runtime-main.b679d28b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
